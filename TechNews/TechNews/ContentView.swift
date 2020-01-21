be79b92aefe45cdb9da82366d597c0cb6b0e27e9
jinxing <jinxing@edaijia-inc.cn>
2020-01-21 22:34:15 +0800

    提交代码

---
+++ TechNews/TechNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TechNews
+//
+//  Created by 金星 on 2020/1/21.
+//  Copyright © 2020 com.corp.jinxing. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
